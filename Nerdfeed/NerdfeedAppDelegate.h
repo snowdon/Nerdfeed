ae19379bb9b61c764182b637c12a527e36632bff
Xiaochun Shen <snowdonshen@gmail.com>
2011-10-20 20:47:33 +0800

    Initial Commit

---
+++ Nerdfeed/NerdfeedAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  NerdfeedAppDelegate.h
+//  Nerdfeed
+//
+//  Created by  Chuns on 11-10-20.
+//  Copyright 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NerdfeedAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
